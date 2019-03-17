ac0d86f26029651e37ee22e36ca5234fb22e63b3
Lucas Thompson <dev@lucas.im>
2019-03-17 19:32:09 +0000

    Enumerator MIDI devices, display in `select` element

--- src/App.js
+++ src/App.js
@@ -3,7 +3,41 @@ import { Piano, KeyboardShortcuts, MidiNumbers } from 'react-piano';
 
 import 'react-piano/dist/styles.css';
 
+const DeviceSelect = ({devices = [], placeholder = `Devices`, ...props}) => (
+  <select {...props}>
+    <option value="">{placeholder}</option>
+    {devices.map(
+      (device, index) => <option key={index} value={index}>{device.name}</option>
+    )}
+  </select>
+);
+
 class App extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      midi: {
+        inputs: [],
+        outputs: []
+      }
+    };
+  }
+
+  componentDidMount() {
+    // bodge: make a helper midi module when we know wtf we are doing / wtf we want
+    (async () => {
+      const devices = await navigator.requestMIDIAccess();
+      this.setState({
+        ...this.state,
+        midi: {
+          inputs: [...devices.inputs.values()],
+          outputs: [...devices.outputs.values()]
+        }
+      });
+    })();
+  }
+
   render() {
     const firstNote = MidiNumbers.fromNote('c3');
     const lastNote = MidiNumbers.fromNote('f5');
@@ -23,6 +57,14 @@ class App extends Component {
           stopNote={(midiNumber) => {}}
           width={1000}
           keyboardShortcuts={keyboardShortcuts}
+        />
+        <DeviceSelect
+          placeholder={`Inputs`}
+          devices={this.state.midi.inputs}
+        />
+        <DeviceSelect
+          placeholder={`Outputs`}
+          devices={this.state.midi.outputs}
         />
       </div>
     );
