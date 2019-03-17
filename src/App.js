a55dbe58e02b67863e68a954b1e8d67461851418
James Dooley <james.dooley@bbc.co.uk>
2019-03-17 17:40:08 +0000

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import { Piano, KeyboardShortcuts, MidiNumbers } from 'react-piano';
+
+import 'react-piano/dist/styles.css';
+
+class App extends Component {
+  render() {
+    const firstNote = MidiNumbers.fromNote('c3');
+    const lastNote = MidiNumbers.fromNote('f5');
+    const keyboardShortcuts = KeyboardShortcuts.create({
+      firstNote: firstNote,
+      lastNote: lastNote,
+      keyboardConfig: KeyboardShortcuts.HOME_ROW,
+    });
+
+    return (
+      <div className="App">
+        <header className="App-header"></header>
+
+        <Piano
+          noteRange={{ first: firstNote, last: lastNote }}
+          playNote={(midiNumber) => {}}
+          stopNote={(midiNumber) => {}}
+          width={1000}
+          keyboardShortcuts={keyboardShortcuts}
+        />
+      </div>
+    );
+  }
+}
+
+export default App;
