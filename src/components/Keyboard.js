6fe3f0ae1f36bc4ef91e233d96a5c4d2514d910e
James Dooley <james.dooley@bbc.co.uk>
2019-03-17 20:31:12 +0000

    Temp: replacing keyboard again lol

--- src/components/Keyboard.js
+++ src/components/Keyboard.js
@@ -1,111 +1,32 @@
-import React from 'react';
-import Piano from 'react-piano-component';
-import './Keyboard.css';
+import React, { Component } from 'react';
 
-function PianoContainer({ children }) {
-  return (
-    <div
-      className={'interactive-piano__piano-container'}
-      onMouseDown={event => event.preventDefault()}
-    >
-      {children}
-    </div>
-  );
-}
+import { Piano, KeyboardShortcuts, MidiNumbers } from 'react-piano';
+import 'react-piano/dist/styles.css';
 
-function AccidentalKey({ isPlaying, text, eventHandlers }) {
-  return (
-    <div className={'interactive-piano__accidental-key__wrapper'}>
-      <button
-        className={`interactive-piano__accidental-key ${
-          isPlaying ? 'interactive-piano__accidental-key--playing' : ''
-        }`}
-        {...eventHandlers}
-      >
-        <div className={'interactive-piano__text'}>{text}</div>
-      </button>
-    </div>
-  );
-}
+class Keyboard extends Component {
+  render() {
+    const firstNote = MidiNumbers.fromNote('c3');
+    const lastNote = MidiNumbers.fromNote('f5');
+    const keyboardShortcuts = KeyboardShortcuts.create({
+      firstNote: firstNote,
+      lastNote: lastNote,
+      keyboardConfig: KeyboardShortcuts.HOME_ROW,
+    });
 
-function NaturalKey({ isPlaying, text, eventHandlers }) {
-  return (
-    <button
-      className={`interactive-piano__natural-key ${
-        isPlaying ? 'interactive-piano__natural-key--playing' : ''
-      }`}
-      {...eventHandlers}
-    >
-      <div className={'interactive-piano__text'}>{text}</div>
-    </button>
-  );
-}
-
-function PianoKey({
-  isNoteAccidental,
-  isNotePlaying,
-  startPlayingNote,
-  stopPlayingNote,
-  keyboardShortcuts,
-}) {
-  function handleMouseEnter(event) {
-    if (event.buttons) {
-      startPlayingNote();
-    }
+    return (
+        <Piano
+          noteRange={{ first: firstNote, last: lastNote }}
+          playNote={(midiNumber) => {
+            // Play a given note - see notes below
+          }}
+          stopNote={(midiNumber) => {
+            // Stop playing a given note - see notes below
+          }}
+          width={1000}
+          keyboardShortcuts={keyboardShortcuts}
+        />
+    );
   }
-
-  const KeyComponent = isNoteAccidental ? AccidentalKey : NaturalKey;
-  const eventHandlers = {
-    onMouseDown: startPlayingNote,
-    onMouseEnter: handleMouseEnter,
-    onTouchStart: startPlayingNote,
-    onMouseUp: stopPlayingNote,
-    onMouseOut: stopPlayingNote,
-    onTouchEnd: stopPlayingNote,
-  };
-  return (
-    <KeyComponent
-      isPlaying={isNotePlaying}
-      text={keyboardShortcuts.join(' / ')}
-      eventHandlers={eventHandlers}
-    />
-  );
 }
 
-export default function InteractivePiano() {
-  return (
-    <PianoContainer>
-      <Piano
-        startNote={'C4'}
-        endNote={'B5'}
-        renderPianoKey={PianoKey}
-        keyboardMap={{
-          Q: 'C4',
-          2: 'C#4',
-          W: 'D4',
-          3: 'D#4',
-          E: 'E4',
-          R: 'F4',
-          5: 'F#4',
-          T: 'G4',
-          6: 'G#4',
-          Y: 'A4',
-          7: 'A#4',
-          U: 'B4',
-          V: 'C5',
-          G: 'C#5',
-          B: 'D5',
-          H: 'D#5',
-          N: 'E5',
-          M: 'F5',
-          K: 'F#5',
-          ',': 'G5',
-          L: 'G#5',
-          '.': 'A5',
-          ';': 'A#5',
-          '/': 'B5',
-        }}
-      />
-    </PianoContainer>
-  );
-}
+export default Keyboard;
