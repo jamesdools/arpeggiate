8d42d543c7e8423dbf672cbbc51ff73716af770a
James Dooley <james.dooley@bbc.co.uk>
2019-03-17 20:47:45 +0000

    Feature: Added soundfont provider to keyboard

--- src/components/Keyboard.js
+++ src/components/Keyboard.js
@@ -1,30 +1,42 @@
 import React, { Component } from 'react';
-
 import { Piano, KeyboardShortcuts, MidiNumbers } from 'react-piano';
+
+import SoundfontProvider from './SoundfontProvider';
+
 import 'react-piano/dist/styles.css';
 
 class Keyboard extends Component {
   render() {
-    const firstNote = MidiNumbers.fromNote('c3');
-    const lastNote = MidiNumbers.fromNote('f5');
+    const audioContext = new (window.AudioContext || window.webkitAudioContext)();
+    const soundfontHostname = 'https://d1pzp51pvbm36p.cloudfront.net';
+
+    const noteRange = {
+      first: MidiNumbers.fromNote('c3'),
+      last: MidiNumbers.fromNote('f4'),
+    };
+
     const keyboardShortcuts = KeyboardShortcuts.create({
-      firstNote: firstNote,
-      lastNote: lastNote,
+      firstNote: noteRange.first,
+      lastNote: noteRange.last,
       keyboardConfig: KeyboardShortcuts.HOME_ROW,
     });
 
     return (
-        <Piano
-          noteRange={{ first: firstNote, last: lastNote }}
-          playNote={(midiNumber) => {
-            // Play a given note - see notes below
-          }}
-          stopNote={(midiNumber) => {
-            // Stop playing a given note - see notes below
-          }}
-          width={1000}
-          keyboardShortcuts={keyboardShortcuts}
-        />
+      <SoundfontProvider
+        instrumentName="acoustic_grand_piano"
+        audioContext={audioContext}
+        hostname={soundfontHostname}
+        render={({ isLoading, playNote, stopNote }) => (
+          <Piano
+            width={640}
+            noteRange={noteRange}
+            playNote={playNote}
+            stopNote={stopNote}
+            disabled={isLoading}
+            keyboardShortcuts={keyboardShortcuts}
+          />
+        )}
+      />
     );
   }
 }
